b6d3f5f5aed1ddbb6280994fa9fa3338c11e9499
shane <sm0862512@otc.edu>
2024-07-11 17:26:15 -0500

    fixed nav bar

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,5 +1,3 @@
-// src/NavBar.tsx
-
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -7,18 +5,19 @@ import Navbar from 'react-bootstrap/Navbar';
 import './navbar.css';
 
 const NavBar = () => {
-
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
                 <Navbar.Brand href="/">Shane</Navbar.Brand>
-                <Nav className="me-auto fw-bold">
-                    <Nav.Link href="/Projects">Projects</Nav.Link>
-                    <Nav.Link href="/About">About</Nav.Link>
-                    <Nav.Link href="https://www.linkedin.com/in/shane-mrad/">LinkedIn</Nav.Link>
-                    <Nav.Link href="/Degrees">Degrees</Nav.Link>
-
-                </Nav>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" /> {/* This is the hamburger button */}
+                <Navbar.Collapse id="basic-navbar-nav"> {/* This wraps the collapsible content */}
+                    <Nav className="me-auto fw-bold">
+                        <Nav.Link href="/Projects">Projects</Nav.Link>
+                        <Nav.Link href="/About">About</Nav.Link>
+                        <Nav.Link href="https://www.linkedin.com/in/shane-mrad/">LinkedIn</Nav.Link>
+                        <Nav.Link href="/Degrees">Degrees</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
