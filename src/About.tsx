69aed19b2dd161b05dfad07f93a27c9e1c96d2a1
shane <sm0862512@otc.edu>
2024-10-17 12:27:03 -0500

    edited index.html

--- src/About.tsx
+++ src/About.tsx
@@ -9,7 +9,7 @@ const About = () => {
             <Container>
                 <h1>About</h1>
                 <p>Hi, I'm Shane Mrad. I am a software developer with a passion for technology. I have experience with Python, Java, Swift, React, and Django. I have a degree in Networking and Computer Science. I am currently working on a Magic the Gathering card scanner using Python. I am also working on a website for my Magic the Gathering collection using React and Django.</p>
-            </Container>
+            </Container>this is a test
         </Container>
     );
 };
